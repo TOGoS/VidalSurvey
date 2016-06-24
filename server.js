39b8b1447b0ab3a318289a853f2db161b79bcc1f
Vidal Quevedo <vidal.e.quevedo@gmail.com>
2016-06-24 00:47:37 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var port = 5000;
+
+function handler() {
+	console.log('Server running on port ' + port + '...');
+}
+
+connect().use(serveStatic(__dirname)).listen(port, handler);
